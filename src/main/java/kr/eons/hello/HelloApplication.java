fedb844fa0c758c837725e0fe7e36b29db944df3
EonSeok Yim <eonse32@gmail.com>
2022-04-20 15:31:14 +0900

    테스트입니다.

--- src/main/java/kr/eons/hello/HelloApplication.java
+++ src/main/java/kr/eons/hello/HelloApplication.java
@@ -17,6 +17,6 @@ public class HelloApplication {
     @GetMapping("/hello")
     public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) {
         return String.format("Hello %s!!!", name);
-        //테스트
+        //테스트aaa
     }
 }
